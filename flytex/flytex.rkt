a7c3560e0ade15cdf62c583b2c34e5e8666fd4da
indrjo <dedej.indrjo@gmail.com>
2023-11-14 14:22:06 +0100

    `flytex` checks if package repo can be reached before trying to install packages

--- flytex/flytex.rkt
+++ flytex/flytex.rkt
@@ -5,7 +5,7 @@
          "tlmgr.rkt")
 
 (command-line
- ;#:program "flytex"
+ #:program "flytex"
  #:args (logfile)
  (let ([not-founds (list-not-founds (string->path logfile))])
    (if (empty? not-founds)
@@ -13,10 +13,12 @@
        (begin
          (say (format "missing files: ~a"
                       (string-join not-founds)))
-         (let ([packages-list (tlmgr-list-package-names not-founds)])
-           (begin
-             (say (format "packages to install: ~a"
-                          (string-join packages-list)))
-             (unless (tlmgr-install packages-list)
-               (say-error "some packages not installed!"))))))))
+         (if (contact-package-repo)
+             (let ([packages (tlmgr-list-package-names not-founds)])
+               (begin
+                 (say (format "packages to install: ~a"
+                              (string-join packages)))
+                 (unless (tlmgr-install packages)
+                   (say-error "some packages not installed!"))))
+             (say-error "cannot contact the package repo!"))))))
 
