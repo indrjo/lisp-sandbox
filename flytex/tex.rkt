0c3a7e786e962bcfbb8d3467802512dc2f87b11f
indrjo <dedej.indrjo@gmail.com>
2023-11-14 19:08:27 +0100

    remove `tex.rkt` source

--- flytex/tex.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(provide tex-command)
-
-(define (tex-command engine main-file)
-  (string-join (list "perl -e 'print \"\n\"x50' |"
-                     "max_print_line=1000" engine main-file)))
-
