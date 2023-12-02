b14f568c0292b9cce1417e444897d215825cb83c
indrjo <dedej.indrjo@gmail.com>
2023-12-02 09:07:32 +0100

    ...

--- flytex/run-shell.rkt
+++ flytex/run-shell.rkt
@@ -5,7 +5,7 @@
 ;; Make the underlying OS run some shell command: wait until it terminates
 ;; and return both stderr and stdout in one string.
 (define (process-output cmd)
-  (match (process (string-append cmd " 2>&1"))
+  (match (process (format "~a 2>&1" cmd))
     [(list std-out std-in _ std-err prc-func)
      (begin0
          (port->string std-out)
