264bedf22bc81e2f383ff3c68f0f8a32c4b03b37
indrjo <dedej.indrjo@gmail.com>
2023-12-18 17:33:30 +0100

    rename from `flytex` to `strelitzia`

--- flytex/info.rkt
+++
@@ -1,9 +0,0 @@
-#lang info
-
-(define collection flytex)
-(define version "0.1")
-(define deps '("base" "parsack"))
-(define pkg-desc "Description Here")
-(define pkg-authors '(indrjo))
-(define license '(MIT))
-(define package-content-state 'binary)
